ce4e18183f446d46ea4bb3e5acc60909f51a59f3
uhm0311 <uhm0311@naver.com>
2019-03-18 17:55:18 +0900

    Added some classes to C# server, and modified documents

---
+++ 소스 코드/C#/PPT Remote Viewer Server/Commands/SetPreviousPageCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPTRemoteViewerServer.Commands
+{
+    public class SetPreviousPageCommand : Command
+    {
+        public override object Execute()
+        {
+            return null;
+        }
+    }
+}
