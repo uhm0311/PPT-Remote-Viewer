091b3d1a624efb87ce0ef60d4b747e1d554b1a76
uhm0311 <uhm0311@naver.com>
2019-03-26 20:55:09 +0900

    Move string literals to resource. Make MainActivity use thread pool instead of NetworkThread.

--- Source code/Android/PPT Remote Viewer/app/src/main/java/com/dku/dblab/android/pptRemoteViewer/MainActivity.java
+++ Source code/Android/PPT Remote Viewer/app/src/main/java/com/dku/dblab/android/pptRemoteViewer/MainActivity.java
@@ -15,22 +15,33 @@ import com.dku.dblab.android.pptRemoteViewer.utils.connections.ConnectionManager
 import com.dku.dblab.android.pptRemoteViewer.utils.observers.ScreenRenewalObserver;
 import com.dku.dblab.android.pptRemoteViewer.utils.observers.subjects.ScreenRenewalNotifier;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends Activity implements ScreenRenewalObserver {
     private ImageView screenViewer = null;
     private EditText ipAddress = null;
-
-    private NetworkThread networkThread = new NetworkThread();
-    private ScreenRenewalNotifier notifier = new ScreenRenewalNotifier();
-
     private final int port = 1282;
+
+    private ConnectionManager connectionManager = null;
+    private ExecutorService threadPool = Executors.newCachedThreadPool();
+    private Runnable startClient, stopClient, setPreviousPage, setNextPage;
+
+    private ScreenRenewalNotifier notifier = new ScreenRenewalNotifier();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        initViews();
+        initConnections();
+
+        notifier.addObserver(this);
+    }
+    
+    private void initViews() {
         View connect = findViewById(R.id.connect);
         View disconnect = findViewById(R.id.disconnect);
         View previousPage = findViewById(R.id.previousPage);
@@ -42,14 +53,14 @@ public class MainActivity extends Activity implements ScreenRenewalObserver {
         previousPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                networkThread.setPreviousPage();
+                setPreviousPage();
             }
         });
 
         nextPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                networkThread.setNextPage();
+                setNextPage();
             }
         });
 
@@ -57,7 +68,7 @@ public class MainActivity extends Activity implements ScreenRenewalObserver {
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                networkThread.startClient();
+                startClient();
             }
         });
 
@@ -65,11 +76,76 @@ public class MainActivity extends Activity implements ScreenRenewalObserver {
         disconnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                networkThread.stopClient();
+                stopClient();
             }
         });
+    }
+    
+    private void initConnections() {
+        startClient = new Runnable() {
+            @Override
+            public void run() {
+                if (connectionManager == null || !connectionManager.isConnected()) {
+                    String ip = ipAddress.getText().toString().trim();
 
-        notifier.addObserver(this);
+                    try {
+                        connectionManager = new ConnectionManager(notifier, ip, port);
+                        connectionManager.startClient();
+                    } catch (Exception e) {
+                        connectionManager = null;
+                        alert(getString(R.string.connection_fail), getString(R.string.connection_fail_title));
+                    }
+                } else {
+                    alert(getString(R.string.connection_already_success), getString(R.string.connection_already_success_title));
+                }
+            }
+        };
+
+        stopClient = new Runnable() {
+            @Override
+            public void run() {
+                if (connectionManager != null || connectionManager.isConnected()) {
+                    try {
+                        connectionManager.stopClient();
+                    } catch (Exception e) {
+                    } finally {
+                        connectionManager = null;
+                    }
+
+                    alert(getString(R.string.connection_lost), getString(R.string.connection_lost_title));
+                } else {
+                    alert(getString(R.string.connection_already_lost), getString(R.string.connection_already_lost_title));
+                }
+            }
+        };
+
+        setPreviousPage = new Runnable() {
+            @Override
+            public void run() {
+                if (connectionManager == null || !connectionManager.isConnected()) {
+                    alert(getString(R.string.connection_need), getString(R.string.connection_need_title));
+                } else {
+                    try {
+                        connectionManager.setPreviousPage();
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        };
+
+        setNextPage = new Runnable() {
+            @Override
+            public void run() {
+                if (connectionManager == null || !connectionManager.isConnected()) {
+                    alert(getString(R.string.connection_need), getString(R.string.connection_need_title));
+                } else {
+                    try {
+                        connectionManager.setNextPage();
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        };
     }
 
     //volume up & down key event
@@ -77,12 +153,12 @@ public class MainActivity extends Activity implements ScreenRenewalObserver {
     public boolean dispatchKeyEvent(KeyEvent event) {
         if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP) {
             if (event.getAction() == KeyEvent.ACTION_DOWN) {
-                networkThread.setPreviousPage();
+                setPreviousPage();
             }
             return true;
         } else if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) {
             if (event.getAction() == KeyEvent.ACTION_DOWN) {
-                networkThread.setNextPage();
+                setNextPage();
             }
             return true;
         } else {
@@ -92,7 +168,7 @@ public class MainActivity extends Activity implements ScreenRenewalObserver {
 
     @Override
     public void onDestroy() {
-        networkThread.stopClient();
+        stopClient();
 
         super.onDestroy();
     }
@@ -121,97 +197,19 @@ public class MainActivity extends Activity implements ScreenRenewalObserver {
         }
     }
 
-    private class NetworkThread {
-        private ConnectionManager connectionManager = null;
-        private Thread thread = null;
+    private void startClient() {
+        threadPool.execute(startClient);
+    }
 
-        private Runnable startClient, stopClient, setPreviousPage, setNextPage;
+    private void stopClient() {
+        threadPool.execute(stopClient);
+    }
 
-        public NetworkThread() {
-            startClient = new Runnable() {
-                @Override
-                public void run() {
-                    if (connectionManager == null || !connectionManager.isConnected()) {
-                        String ip = ipAddress.getText().toString().trim();
+    private void setPreviousPage() {
+        threadPool.execute(setPreviousPage);
+    }
 
-                        try {
-                            connectionManager = new ConnectionManager(notifier, ip, port);
-                            connectionManager.startClient();
-                        } catch (Exception e) {
-                            connectionManager = null;
-                            alert("<b><font color=#ff0000>CHECK</font></b> your computer <b><font color=#ff0000>BEFORE CONNECT</font></b>.", "Unavailable connection!");
-                        }
-                    } else {
-                        alert("Your device <b><font color=#ff0000>CONNECTION</font></b> to server has been <b><font color=#ff0000>ALREADY CONNECTED</font></b>.", "Already connected!");
-                    }
-                }
-            };
-
-            stopClient = new Runnable() {
-                @Override
-                public void run() {
-                    if (connectionManager != null || connectionManager.isConnected()) {
-                        try {
-                            connectionManager.stopClient();
-                        } catch (Exception e) {
-                        } finally {
-                            connectionManager = null;
-                        }
-
-                        alert("Your device <b><font color=#ff0000>CONNECTION</font></b> to server has been <b><font color=#ff0000>LOST</font></b>.", "Disconnected!");
-                    } else {
-                        alert("Your device <b><font color=#ff0000>CONNECTION</font></b> to server has been <b><font color=#ff0000>ALREADY LOST</font></b>.", "Already not connected!");
-                    }
-                }
-            };
-
-            setPreviousPage = new Runnable() {
-                @Override
-                public void run() {
-                    if (connectionManager == null || !connectionManager.isConnected()) {
-                        alert("You need to <b><font color=#ff0000>CONNECT</font></b> your device to computer server <b><font color=#ff0000>BEFORE</font></b> control PPT.", "Not connected!");
-                    } else {
-                        try {
-                            connectionManager.setPreviousPage();
-                        } catch (Exception e) {
-                        }
-                    }
-                }
-            };
-
-            setNextPage = new Runnable() {
-                @Override
-                public void run() {
-                    if (connectionManager == null || !connectionManager.isConnected()) {
-                        alert("You need to <b><font color=#ff0000>CONNECT</font></b> your device to computer server <b><font color=#ff0000>BEFORE</font></b> control PPT.", "Not connected!");
-                    } else {
-                        try {
-                            connectionManager.setNextPage();
-                        } catch (Exception e) {
-                        }
-                    }
-                }
-            };
-        }
-
-        public void startClient() {
-            thread = new Thread(startClient);
-            thread.start();
-        }
-
-        public void stopClient() {
-            thread = new Thread(stopClient);
-            thread.start();
-        }
-
-        public void setPreviousPage() {
-            thread = new Thread(setPreviousPage);
-            thread.start();
-        }
-
-        public void setNextPage() {
-            thread = new Thread(setNextPage);
-            thread.start();
-        }
+    private void setNextPage() {
+        threadPool.execute(setNextPage);
     }
 }
